58df14f494b51e49cf49da30924cf4f3dd3a570f
Jessica0000000 <jessicaflower12@outlook.com>
2024-09-30 11:09:59 -0400

    done

--- src/App.java
+++ src/App.java
@@ -6,15 +6,23 @@ public class App extends PApplet{
     }
 
     public void setup(){
-        
+        background(119, 211, 242);
     }
 
     public void settings(){
-        
+        size(800, 600);  
     }
 
+    int myHealth = 100;
+    int mHealth =10;
+
     public void draw(){
-       
+        background(119, 211, 242);
+    fill(0);
+        ellipse(400,280,300,300);
+
+        fill(0);  // Set the text color to black
+        text(mouseX + ", " + mouseY, 10, 20);
 
     }
 }
