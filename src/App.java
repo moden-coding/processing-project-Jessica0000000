90e7f06706b4ad3e67a0a4ee851c53b58fdc88b9
Jessica0000000 <jessicaflower12@outlook.com>
2024-10-14 19:31:38 -0400

    done with project

--- src/App.java
+++ src/App.java
@@ -1,17 +1,17 @@
 import processing.core.*;
 
-public class App extends PApplet{
-    public static void main(String[] args)  {
+public class App extends PApplet {
+    public static void main(String[] args) {
         PApplet.main("App");
     }
 
-    public void setup(){
+    public void setup() {
         background(119, 211, 242);
-        
+
     }
 
-    public void settings(){
-        size(800, 600);  
+    public void settings() {
+        size(800, 600);
     }
 
     int scene = 0;
@@ -20,106 +20,131 @@ public class App extends PApplet{
     int experience = 0;
     int level = 1;
     int color = 211;
-    boolean isVisible = true; 
-
-
-    public void draw(){
-
-        if (scene==0 ){
-            background(119, color, 242);
-            color= 10;
-
-            playButton();
-            if (mousePressed){
-                scene++;
-            }
-        }
-        if(scene == 1){
-        
-    
-        //line(x1, y1, x2, y2);
-        background(119, color, 242);
-        strokeWeight(5);
-        line(197,319,140,600);
-
-        
-        fill(0);  // Set the text color to black
-        text(mouseX + ", " + mouseY, 10, 20);
-        
-       if(isVisible){
-        if (mHealth>0){
-            strokeWeight(2);
-        fill(255);
-        ellipse(400,280,300,300);
-        fill(0);
-        ellipse(348,230,70,70);
-        fill(0);
-        ellipse(450,230,70,70);
-        }
-        
-
-        if(myHealth>0){   
-            mAttack();
-            }
-    }
-
-    if(myHealth==0){
-        youDied();
-    }
-}
-    }
-    public void mousePressed(){
+    boolean isVisible = false;
+    int frameWhenClicked = -30;
   
-       if (mHealth>0){
-        mHealth-=1;
-      
-        System.out.println("Monster's health is " + mHealth);
-       
-    }else if(mHealth==0){
 
-        experience++;
-        if (color<250){
-            color+=20;
-        }else if(color>250){
-            color=0;
+    public void draw() {
+
+       //System.out.println("isVisible is " + isVisible);
+     //  System.out.println("scene is " + scene);
+
+        
+            
+            if (scene == 0) {
+                background(119, color, 242);
+               
+                playButton();
+                
+                color = 10;
+
+                if (mouseX > 200 && mouseX < 600 && mouseY < 330 && mouseY > 230) {
+                    if (mousePressed) {
+                        scene++;
+                        isVisible = true;
+                    }
+                }
+            
+            if (scene == 1) {
+
+                // line(x1, y1, x2, y2);
+                background(119, color, 242);
+                strokeWeight(5);
+                line(197, 319, 140, 600);
+
+                fill(0); // Set the text color to black
+                text(mouseX + ", " + mouseY, 10, 20);
+
+                fill(255);
+                textSize(50);
+             
+              text("Click to attack", 409, 135);
+
+                if (myHealth > 0 && mHealth > 0) {
+                    mAttack();
+                }
+
+                if (mHealth > 0) {
+                    strokeWeight(2);// this is the monster
+                    fill(255);
+                    ellipse(400, 280, 300, 300);
+                    fill(0);
+                    ellipse(348, 230, 70, 70);
+                    fill(0);
+                    ellipse(450, 230, 70, 70);
+                }
+            }
+            
         }
-        mHealth=10;
-        System.out.println("Monster's health is " + mHealth);
-        System.out.println("Experience is " + experience);
-        System.out.println("Color is " + color);
+            
+
+            if (myHealth == 0) {
+                youDied();
+            }
+        
     }
 
-}
+    public void mousePressed() {
+        frameWhenClicked = frameCount;
 
-public void mAttack(){
-    delay (1000);
+        if (mHealth > 0 && scene == 1) {
+            mHealth -= 1;
 
-    myHealth-=1;
-    System.out.println("I attacked");
-    System.out.println("Your health is " + myHealth);
-}
+            System.out.println("Monster's health is " + mHealth);
+            System.out.println("Experience is " + experience);
+            System.out.println("Color is " + color);
+            System.out.println("scene is " + scene);
 
-public void youDied(){
+        } else if (mHealth == 0) {
 
-    scene++;
-    isVisible = false;
-    background(119, 211, 242);
+            monsterDead();
+        }
 
-    fill(0);  // Set the text color to black
+    }
+
+    public void monsterDead() {
+        myHealth = 10;
+        experience++;
+        if (color < 250) {
+            color += 20;
+        } else if (color > 250) {
+            color = 0;
+        }
+        mHealth += 5;
+        isVisible = true;
+
+    }
+
+    public void mAttack() {
+        delay(1000);
+
+        myHealth -= 1;
+        System.out.println("I attacked");
+        System.out.println("Your health is " + myHealth);
+    }
+
+    public void youDied() {
+
+        scene++;
+        isVisible = false;
+        background(119, 211, 242);
+
+        fill(0); // Set the text color to black
         text(mouseX + ", " + mouseY, 10, 20);
-    textSize(100);
-    text("YOU DIED", 385, 237);
+        textSize(100);
+        text("YOU DIED", 385, 237);
 
-}
+    }
 
-public void playButton(){
-    fill(0);
-    rect(200, 230, 400, 100);
-    textSize(10);
-    fill(0);  // Set the text color to black
+    public void playButton() {
+        fill(0);
+        rect(200, 230, 400, 100);
+        textSize(10);
         text(mouseX + ", " + mouseY, 10, 20);
-    textSize(50);
+        textSize(50);
         fill(255);
-        text ("Play", 360, 290);
-}
+        text("Play", 360, 290);
+        
+
+    }
 }
